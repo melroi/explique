d8a4720bdbbc3e205093fa04320db083fc2223a6
melroi <125811920+melroi@users.noreply.github.com>
2025-01-29 20:14:05 +0100

    up copypasta

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ async def on_message(message):
     print(f"[LOG] Message reçu de {message.author}: {message.content}")  # Log le message reçu
 
     if message.content.lower() == "explique":
-        response = "Explique hahaha pff ouais c'est un peu chiant les gars en gros Luden..."
+        response = "explique hahaha pff ouais c'est un peu chiant les gars en gros Luden c'est un mythique qui donne de la péné magique et donc en en gros ça donne 6 de péné magique flat donc à 2 items complets.. donc il a 10 de péné flat donc il monte à 16, il a les bottes ça fait 18. Donc 16+18 ça fait 34 si jdis pas de conneries donc 34 + il avait shadow flame donc il a 44 et il a 44 et après du coup le void staff faut faire 44 divisé par 0.6 en gros il fait des dégats purs à un mec jusqu'à 73 d'rm j'avais dit 70 dans le cast à peu près et en gros bah les mecs ils ont pas 70 d'rm parce que globalement y'a eu un patch, en gros y'a le patch qui fait 0.8 d'rm sur les carrys et en gros de base sur lol y'avait pas ça et en gros la botlane va jamais prendre de la rm en lane en tout cas pas beaucoup donc c'est pas ouf en vrai j'pense que son item est nul donc en vrai j'pense soit il enlève shadow flame soit le void staff mais j'pense qu'il vaut mieux enlever shadow flame"
         await message.channel.send(response)
         print(f"[LOG] Réponse envoyée dans #{message.channel}: {response[:50]}...")  # Log la réponse
 
